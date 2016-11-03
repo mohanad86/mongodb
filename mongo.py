06c3a10d2da47569ca123a87558fc8c4194f2a4a
Mohanad Aly <capmohanad@hotmail.com>
2016-11-03 23:07:04 +0200

    Adding the insert method

--- mongo.py
+++ mongo.py
@@ -1,19 +1,31 @@
 import pymongo
 from pymongo import MongoClient
 client = MongoClient('localhost', 54321)
+print (client)
 db = client.clients
-collection = db.clients
 db.clients.count()
 clients = db.clients
 clients.find()
 
 
+
+for k in range(6000):
+    clients = db.clients.insert({"birthdate":"1970-01-01"});
+print (k)
+
+for l in range(6000):
+    clients = db.clients.insert({"birthdate":"1995-01-01"});
+print(l)
+
+for i in range(100):
+   clients = db.clients.insert({"name":"Tom"});
+print(i)
+
+for j in range(100):
+   clients = db.clients.insert({"name":"Tom hanks"});
+print(j)
+
+
+
 print(db.clients.count())
-
-for i in range(5):
-    clients = db.clients.insert({'date': 'name'})
-    
-print(clients)
-
-
 
